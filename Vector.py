00c7c86bd9bbb208f10978273bb234dd495891df
BLourence <ben@lourence.com>
2018-07-22 12:37:08 +0100

    Updated object to reflect python naming conventions, wip for line class to find intersections

--- Vector.py
+++ Vector.py
@@ -8,8 +8,8 @@ class Vector(object):
         try:
            if not coordinates:
                 raise ValueError
-            self.coordinates = tuple([Decimal(x) for x in coordinates])
-            self.dimension = len(coordinates)
+           self.coordinates = tuple([Decimal(x) for x in coordinates])
+           self.dimension = len(coordinates)
  
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -22,23 +22,26 @@ class Vector(object):
  
     def __eq__(self, vec):
         return self.coordinates == vec.coordinates
+    
+    def iter(self): 
+        return self.coordinates.iter()
    
-    def Plus(self, vec):
+    def plus(self, vec):
         new_coordinates = [x + y for x,y in zip(self.coordinates, vec.coordinates)]
         return Vector(new_coordinates)
            
-    def Minus(self, vec):
+    def minus(self, vec):
         new_coordinates = [x - y for x,y in zip(self.coordinates, vec.coordinates)]
         return Vector(new_coordinates)
            
-    def Multiply(self, scalar):
+    def multiply(self, scalar):
         new_coordinates = [x * scalar for x in self.coordinates]               
         return Vector(new_coordinates)
  
-    def Magnitude(self):
+    def magnitude(self):
         return Decimal(sqrt(sum([x *x  for x in self.coordinates])))
  
-    def Normalisation(self):
+    def normalisation(self):
         try:
             normalisationFactor = Decimal("1.0") / self.Magnitude()
             return Vector([x * normalisationFactor for x in self.coordinates])
@@ -46,32 +49,32 @@ class Vector(object):
         except ZeroDivisionError:
             raise Exception("Cannot normalise a vector with zero values")
    
-    def DotProduct(self, vec):
+    def dotProduct(self, vec):
         return sum(x * y for x,y in zip(self.coordinates, vec.coordinates))        
         
-    def Angle(self, vec, asDegrees = False):
+    def angle(self, vec, asDegrees = False):
         # handling 0 vector is still required
         cosTheta = self.DotProduct(vec) / (self.Magnitude() * vec.Magnitude())
         radians = acos(round(cosTheta, 4))       
         return radians if not asDegrees else degrees(radians)
        
-    def IsParallel(self, vec):
+    def is_parallel(self, vec):
         # if vec is 0 then is parallel
         if self.IsZero() or vec.IsZero():
             return True      
         return Decimal(self.Angle(vec)) in [0, pi]
  
-    def IsOrthogonal(self, vec):
+    def is_orthogonal(self, vec):
         # if vec is 0 then is parallel
         if self.IsZero() or vec.IsZero():
             return True     
         return round(self.DotProduct(vec),3) == 0
    
-    def IsZero(self):
+    def is_zero(self):
         return self.Magnitude() == 0
    
     
-    def ProjectOnto(self, basisVector):
+    def project_onto(self, basisVector):
         """
         This is the parallel element of projected vector onto basis vector
         """
@@ -79,14 +82,14 @@ class Vector(object):
         weight = self.DotProduct(normalisation)
         return normalisation.Multiply(weight)
    
-    def PerpendicularToProjectedVector(self, basisVector):
+    def perpendicular_to_projectedVector(self, basisVector):
        """
-        This is the perpendicular element of projected vector onto basis vector
-        """
-        projectedVector = self.ProjectOnto(basisVector)
-        return self.Minus(projectedVector)
+       This is the perpendicular element of projected vector onto basis vector
+       """
+       projectedVector = self.ProjectOnto(basisVector)
+       return self.Minus(projectedVector)
    
-    def CrossProduct(self, vec):
+    def crossProduct(self, vec):
         x1 = self.coordinates[0]
         y1 = self.coordinates[1]
         z1 = self.coordinates[2]
@@ -95,13 +98,13 @@ class Vector(object):
         z2 = vec.coordinates[2]      
         return Vector([y1*z2 - y2*z1, -(x1*z2 - x2*z1), x1*y2 - x2*y1])
    
-    def AreaOfParallelogram(self, vec):
+    def area_of_parallelogram(self, vec):
         """
         Area of parallelogram from the computed cross product of two vectors
         """
         crossProd = self.CrossProduct(vec)
         return crossProd.Magnitude()
    
-    def AreaOfTriangle(self, vec):
+    def area_of_triangle(self, vec):
         crossProd = self.CrossProduct(vec)
         return crossProd.Magnitude() / Decimal("2.0")
